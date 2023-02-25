77e9808d726c671928af7bd63297f402919ce6d3
Humaid Dikko <105851025+humaidd2@users.noreply.github.com>
2023-02-25 22:47:29 +0200

    day 28 commit. pomodoro gui app

--- Day 23 [Files, Directories and Paths]/main.py
+++ Day 23 [Files, Directories and Paths]/main.py
@@ -1,13 +1,13 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
 with open("./Input/Names/invited_names.txt") as data:
     unfiltered_names = data.readlines()
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 
 for name in unfiltered_names:
@@ -20,4 +20,3 @@ for name in unfiltered_names:
     final_letter = text.replace("[name]", name)
     with open(f"./Output/ReadyToSend/{name}.txt", mode="w") as content:
         content.write(final_letter)
-
